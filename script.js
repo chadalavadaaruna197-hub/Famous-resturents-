e12d2338d43b3264873c6a53e8d29ab47656c18d
Aruna .Ch <chadalavadaaruna197@gmail.com>
2026-02-16 13:27:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function showMessage() {
+    alert("Your table booking request is received!");
+}
+function bookTable(restaurantName) {
+    alert(
+        "Table booking request sent to " +
+        restaurantName +
+        ". Restaurant will contact you soon!"
+    );
+}
